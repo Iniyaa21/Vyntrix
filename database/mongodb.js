a716ae81fa9e16777849e0668723be0c28482be8
Iniyaa21 <iniyaapaari@gmail.com>
2025-12-07 15:01:06 +0530

    feat: connect database at app startup and add environment-based log

--- database/mongodb.js
+++ database/mongodb.js
@@ -10,6 +10,8 @@ if (!DB_URI) {
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(DB_URI);
+
+    console.log(`Connected to database in ${NODE_ENV} mode`);
   } catch (error) {
     console.error("Error connecting to database: ", error);
     process.exit(1);
