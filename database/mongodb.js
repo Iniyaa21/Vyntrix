664bcb1094d4b08602c1ab418fdbfdcfff2053ed
Iniyaa21 <iniyaapaari@gmail.com>
2025-12-07 14:54:05 +0530

    feat: implement MongoDB connection file using mongoose and env variables

---
+++ database/mongodb.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+  throw new Error(
+    `Please define the MongoDB Atlas DB_URI environment variable inside the .env.${NODE_ENV}.local`
+  );
+}
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(DB_URI);
+  } catch (error) {
+    console.error("Error connecting to database: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectToDatabase;
