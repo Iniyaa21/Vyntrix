32ad0b02b9a06c69cbe87aced6549c029ea2a699
Iniyaa21 <iniyaapaari@gmail.com>
2025-12-16 20:58:02 +0530

    chore: add Arcjet environment variables to config

--- config/env.js
+++ config/env.js
@@ -2,5 +2,12 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const { PORT, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN, DB_URI } =
-  process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  DB_URI,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
