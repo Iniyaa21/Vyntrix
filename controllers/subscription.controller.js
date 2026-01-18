d8a9aabcde36bf91bec7c0e3bee12f04c0469d3d
Iniyaa21 <iniyaapaari@gmail.com>
2026-01-18 21:21:48 +0530

    feat: implement updateSubscription logic

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -67,7 +67,7 @@ export const getSubscriptionDetails = async (req, res, next) => {
 
     if (req.user.id !== subscriptionOwnerId && req.user.role !== "admin") {
       const error = new Error(
-        "You do not have permission to access this subscription details"
+        "You do not have permission to access this subscription details",
       );
       error.statusCode = 403;
       throw error;
@@ -76,6 +76,60 @@ export const getSubscriptionDetails = async (req, res, next) => {
     res.status(200).json({
       success: true,
       data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const subscriptionOwnerId = subscription.user.toString();
+
+    if (req.user.id !== subscriptionOwnerId && req.user.role !== "admin") {
+      const error = new Error(
+        "You do not have permission to update this subscription's details",
+      );
+      error.statusCode = 403;
+      throw error;
+    }
+
+    if (Object.keys(req.body).length === 0) {
+      const error = new Error("No fields provided for update");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const allowedFields = ["status", "paymentMethod"];
+
+    const hasForbiddenField = Object.keys(req.body).some(
+      (field) => !allowedFields.includes(field),
+    );
+
+    if (hasForbiddenField) {
+      const error = new Error("Only payment method and status are allowed to be updated");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const updatedSubscription = await Subscription.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true, runValidators: true },
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Subscription has been updated successfully",
+      data: updatedSubscription,
     });
   } catch (error) {
     next(error);
